05bd2d43f1a559477fb871dbf49e87d84c6d8ca3
Jeanchami21 <chamijean21@gmail.com>
2020-09-05 15:25:56 +0200

    dynamisation des annonces

--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -2,11 +2,15 @@
 
 namespace App\Entity;
 
+use Cocur\Slugify\Slugify;
 use App\Repository\AdRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=AdRepository::class)
+ * @ORM\HasLifecycleCallbacks
  */
 class Ad
 {
@@ -52,6 +56,29 @@ class Ad
      */
     private $rooms;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Image::class, mappedBy="ad", orphanRemoval=true)
+     */
+    private $images;
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+    }
+
+    /**
+     * Permet d'initialiser le slug
+     * Undocumented function
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     * @return void
+     */
+    public function initializeSlug(){
+        if (empty($this->slug)) {
+           $slugify = new Slugify();
+           $this->slug = $slugify->slugify($this->title);
+        }
+    }
     public function getId(): ?int
     {
         return $this->id;
@@ -137,6 +164,37 @@ class Ad
     public function setRooms(int $rooms): self
     {
         $this->rooms = $rooms;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Image[]
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+            $image->setAd($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->images->contains($image)) {
+            $this->images->removeElement($image);
+            // set the owning side to null (unless already changed)
+            if ($image->getAd() === $this) {
+                $image->setAd(null);
+            }
+        }
 
         return $this;
     }
