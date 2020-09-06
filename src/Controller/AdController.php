1b1ef66fdd72182db0846a68aaad4be68848b693
Jeanchami21 <chamijean21@gmail.com>
2020-09-06 22:30:54 +0200

    add.flashes

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -8,7 +8,6 @@ use App\Repository\AdRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -38,12 +37,17 @@ class AdController extends AbstractController
 
         $form->handleRequest($request);
       
+     
         if ($form->isSubmitted()&& $form->isValid()) {
             //$manager = $this->getDoctrine()->getManager();
 
             $manager->persist($ad);
             $manager->flush();
 
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
             return $this->redirectToRoute('ads_show', [
                 'slug' => $ad->getSlug()
             ]);
